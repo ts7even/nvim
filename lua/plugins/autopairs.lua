c7ffe0b25eb11a54fed1a133e2e986f99b4e0e95
Trevor Seibert <trevorpaulseibert@gmail.com>
2025-04-12 22:45:29 -0500

    Created markdown formatter

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,22 @@
 return {
-    'windwp/nvim-autopairs',
+    "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
+    opts = {},
+
+    config = function(_, opts)
+        local npairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
+
+        npairs.setup(opts)
+
+        -- Add custom rule for /* */
+        npairs.add_rules({
+            Rule("/*", "*/", { "c", "cpp", "java", "javascript", "typescript", "rust", "go" }):with_move(function(opts)
+                return opts.char == "*"
+            end):with_pair(function(opts)
+                local prev_char = opts.line:sub(opts.col - 1, opts.col - 1)
+                return prev_char ~= "*"
+            end),
+        })
+    end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,27 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier.with({
-					filetypes = { "javascript", "typescript", "markdown" },
-					extra_args = { "--print-width", "80" },
-				}),
-				null_ls.builtins.formatting.markdownlint,
-				null_ls.builtins.formatting.cmake_format,
-				null_ls.builtins.formatting.sqlfmt,
-				null_ls.builtins.formatting.yamlfmt,
-				null_ls.builtins.diagnostics.markdownlint,
-				null_ls.builtins.completion.spell,
-			},
-		})
-		vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, {})
-	end,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier.with({
+                    filetypes = { "javascript", "typescript" },
+                }),
+
+                null_ls.builtins.formatting.mdformat.with({
+                    filetypes = { "markdown" },
+                    extra_args = { "--wrap", "80" },
+                }),
+                null_ls.builtins.formatting.markdownlint,
+                null_ls.builtins.formatting.cmake_format,
+                null_ls.builtins.formatting.sqlfmt,
+                null_ls.builtins.formatting.yamlfmt,
+                null_ls.builtins.diagnostics.markdownlint,
+                null_ls.builtins.completion.spell,
+            },
+        })
+        vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, {})
+    end,
 }
