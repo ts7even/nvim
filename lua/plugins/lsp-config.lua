881889817b238dc3cd9201b4c6aeb2567dcfd4a9
Trevor Seibert <trevorpaulseibert@gmail.com>
2025-07-30 22:00:17 -0500

    Added a few things

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,176 +1,196 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "bashls",
-                    "clangd",
-                    "cssls",
-                    "dockerls",
-                    "gopls",
-                    "html",
-                    "ts_ls",
-                    "marksman",
-                    "ruff",
-                    "pyright",
-                    "svelte",
-                    "sqlls",
-                    "taplo",
-                    "vuels",
-                    "yamlls",
-                },
-            })
-        end,
-    },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"bashls",
+					"clangd",
+					"cssls",
+					"dockerls",
+					"gopls",
+					"html",
+					"ts_ls",
+					"marksman",
+					"ruff",
+					"pyright",
+					"svelte",
+					"sqlls",
+					"taplo",
+					"vuels",
+					"yamlls",
+				},
+			})
+		end,
+	},
 
-    {
-        "neovim/nvim-lspconfig",
+	{
+		"neovim/nvim-lspconfig",
 
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		config = function()
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
 
-            -- for ruff and pyright coexistance
-            local on_attach = function(client, bufnr)
-                local function bufmap(mode, lhs, rhs)
-                    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { noremap = true, silent = true })
-                end
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-                -- Use Ruff for formatting
-                bufmap("n", "<leader>fc", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>")
+			-- for ruff and pyright coexistance
+			local on_attach = function(client, bufnr)
+				local function bufmap(mode, lhs, rhs)
+					vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { noremap = true, silent = true })
+				end
 
-                -- Disable hover for Ruff to let Pyright handle it
-                if client.name == "ruff" then
-                    client.server_capabilities.hoverProvider = false
-                end
-            end
+				-- Use Ruff for formatting
+				bufmap("n", "<leader>fc", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>")
 
-            local lspconfig = require("lspconfig")
+				-- Disable hover for Ruff to let Pyright handle it
+				if client.name == "ruff" then
+					client.server_capabilities.hoverProvider = false
+				end
+			end
 
-            -- Setup for Ruff
+			local lspconfig = require("lspconfig")
 
-            -- Pyright: for type checking, hover, definitions
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {
-                    pyright = {
-                        disableOrganizeImports = true,
-                    },
-                    python = {
-                        analysis = {
-                            ignore = { "*" }, -- Let Ruff handle linting
-                        },
-                    },
-                },
-            })
+			-- Setup for Ruff
 
-            -- Ruff: for linting + formatting
-            lspconfig.ruff.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {
-                    ruff = {
-                        format = { enable = true },
-                    },
-                },
-            })
+			-- Pyright: for type checking, hover, definitions
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					pyright = {
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							ignore = { "*" }, -- Let Ruff handle linting
+						},
+					},
+				},
+			})
 
-            lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.bashls.setup({ capabilities = capabilities })
-            lspconfig.clangd.setup({ capabilities = capabilities })
-            lspconfig.cssls.setup({ capabilities = capabilities })
-            lspconfig.dockerls.setup({ capabilities = capabilities })
-            lspconfig.gopls.setup({ capabilities = capabilities })
-            lspconfig.html.setup({ capabilities = capabilities })
-            lspconfig.ts_ls.setup({ capabilities = capabilities })
-            lspconfig.marksman.setup({ capabilities = capabilities })
-            lspconfig.svelte.setup({ capabilities = capabilities })
-            lspconfig.taplo.setup({ capabilities = capabilities })
-            lspconfig.vuels.setup({ capabilities = capabilities })
-            lspconfig.yamlls.setup({ capabilities = capabilities })
+			-- Ruff: for linting + formatting
+			lspconfig.ruff.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					ruff = {
+						format = { enable = true },
+					},
+				},
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.bashls.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.dockerls.setup({ capabilities = capabilities })
+			lspconfig.gopls.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.ts_ls.setup({ capabilities = capabilities })
+			lspconfig.marksman.setup({ capabilities = capabilities })
+			lspconfig.svelte.setup({ capabilities = capabilities })
+			lspconfig.taplo.setup({ capabilities = capabilities })
+			lspconfig.vuels.setup({ capabilities = capabilities })
+			lspconfig.yamlls.setup({ capabilities = capabilities })
 
-    {
-        "mrcjkb/rustaceanvim",
-        version = "^5",
-        lazy = false,
-        -- RustAnalyzer is handled automatically by rustacean.nvim
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 
-    {
-        "hrsh7th/cmp-nvim-lsp",
-    },
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5",
+		lazy = false,
+		config = function()
+			vim.g.rustaceanvim = {
+				server = {
+					on_attach = function(client, bufnr)
+						-- Use RustLsp commands for Rust-specific actions
+						vim.keymap.set("n", "<leader>ca", function()
+							vim.cmd.RustLsp("codeAction")
+						end, { buffer = bufnr, desc = "Code actions" })
+					end,
+					capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				},
+			}
+		end,
+	},
 
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-        },
-    },
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
 
-    {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = {
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif require("luasnip").expand_or_jumpable() then
-                            require("luasnip").expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif require("luasnip").jumpable(-1) then
-                            require("luasnip").jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
-                },
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" }, -- LSP source
-                    { name = "luasnip" }, -- Snippet source
-                }, {
-                    { name = "buffer" }, -- Buffer source
-                }),
-            })
-        end,
-    },
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = {
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif require("luasnip").expand_or_jumpable() then
+							require("luasnip").expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif require("luasnip").jumpable(-1) then
+							require("luasnip").jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
+				},
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" }, -- LSP source
+					{ name = "luasnip" }, -- Snippet source
+				}, {
+					{ name = "buffer" }, -- Buffer source
+				}),
+			})
+		end,
+	},
 }
