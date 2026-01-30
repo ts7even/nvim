e8b72c82e8565dc09b8a237ec7e5c06f4af12af5
Trevor Seibert <trevorpaulseibert@gmail.com>
2026-01-30 15:28:59 -0600

    Updated oil command

--- lua/plugins/utils/oil.lua
+++ lua/plugins/utils/oil.lua
@@ -2,7 +2,7 @@ return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-        { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+        { "<leader>e", "<cmd>Oil<cr>", desc = "Open parent directory" },
     },
     cmd = "Oil",
     opts = {
