4d1e28c2804caec2c66847dc52e4deacef46fb66
Trevor Seibert <trevorpaulseibert@gmail.com>
2026-01-30 12:46:37 -0600

    Removed utils

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
 -- Bootstrap lazy.nvim plugin manager and load configuration
 require("settings")
-require("utils")
 require("plugins")
-
--- lua/settings.lua
+++ lua/settings.lua
@@ -44,3 +44,21 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         vim.lsp.buf.format({ async = false })
     end,
 })
+
+-- Spell suggestions: small floating window, bottom-left, 5 choices
+vim.keymap.set("n", "<leader>ss", function()
+    local word = vim.fn.expand("<cword>")
+    local suggestions = vim.fn.spellsuggest(word, 5)
+    if #suggestions == 0 then
+        vim.notify("No suggestions for: " .. word, vim.log.levels.WARN)
+        return
+    end
+    vim.ui.select(suggestions, {
+        prompt = "Spelling: " .. word,
+    }, function(choice)
+        if choice then
+            vim.cmd("normal! ciw" .. choice)
+            vim.cmd("stopinsert")
+        end
+    end)
+end, { desc = "Spelling suggestions" })
